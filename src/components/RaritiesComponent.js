12f3cc9c3a113be90570545a6f824c64b9bd3b34
Gabriel Corradini Siqueira <56658740+gabrielcorradinis@users.noreply.github.com>
2023-05-16 10:16:54 -0300

    add disconnect callback

--- src/components/RaritiesComponent.js
+++ src/components/RaritiesComponent.js
@@ -8,14 +8,12 @@ export default class RaritiesComponent extends HTMLElement {
         this.data = [];
         this.fetchData = this.fetchData.bind(this);
         this.render = this.render.bind(this);
-        
+        this.listeners = [];
     }
-
     connectedCallback() {
         this.render();
         this.fetchData();
     }
-
     fetchData() {
         fetch('http://reshade.io:1234')
         .then(response => response.json())
@@ -23,84 +21,100 @@ export default class RaritiesComponent extends HTMLElement {
             this.data = data;
             this.render();
             this.addEventListeners();
-        });
+        })
+        .catch(error => {
+            console.error("Error fetching data: ", error);
+            this.shadowRoot.innerHTML = 'Error loading data.';
+        });;
     }
-
     render() {
-    if (!this.data.length) {
-        this.shadowRoot.innerHTML = 'Loading...';
-        return;
-    }
-
-    this.shadowRoot.innerHTML = `
-        <style>
-            li {
-                color: gray;
-                display: flex;
-                align-items: center;
-            }
-            .checkbox {
-                appearance: none;
-                width: 24px;
-                height: 24px;
-                margin: 12px;
-                position: relative;
-                cursor: pointer;
-            }
-            .checkbox::before {
-                content: "";
-                width: 100%;
-                height: 100%;
-                border: 0.5px solid gray;
-                border-radius: 4px; 
-                position: absolute;
-            }
-            .checkbox:checked::before {
-                background: linear-gradient(rgb(62, 174, 255) -4.13%, rgb(60, 244, 200) 97.72%);
-            }
-            .checkbox::after {
-                content: "";
-                display: none;
-                width: 100%;
-                height: 100%;
-                background: url(${RaritiesComponent.checkIcon}) no-repeat;
-                background-size: 80%;
-                background-position: 70% 55%;
-                position: absolute;
-            }
-            .checkbox:checked::after {
-                display: block;
-            }
-        </style>
-        <ul>
-            ${this.data.map((item, index) => `
-                <li>
-                    <input type="checkbox" class="checkbox" id="item-${index}">
-                    <span id="text-${index}">${item[0]}</span>
-                </li>
-            `).join('')}
-        </ul>`;
-    }
-    addEventListeners() {
-        this.data.forEach((item, index) => {
-            this.shadowRoot.querySelector(`#item-${index}`).addEventListener('change', (e) => {
-                this.shadowRoot.querySelector(`#text-${index}`).style.color = e.target.checked ? `#${item[1]}` : 'gray';
+        if (!this.data.length) {
+            this.shadowRoot.innerHTML = 'Loading...';
+            return;
+        }
+        this.shadowRoot.innerHTML = `
+            ${this.getStyles()}
+            <ul>
+                ${this.data.map((item, index) => `
+                    <li>
+                        <input type="checkbox" class="checkbox" id="item-${index}">
+                        <span id="text-${index}">${item[0]}</span>
+                    </li>
+                `).join('')}
+            </ul>`;
+        }
+        addEventListeners() {
+            this.data.forEach((item, index) => {
+              const checkboxElement = this.shadowRoot.querySelector(`#item-${index}`);
+              const textElement = this.shadowRoot.querySelector(`#text-${index}`);
+              const callback = (e) => {  // Armazena a função de callback
+                textElement.style.color = e.target.checked ? `#${item[1]}` : 'gray';
                 this.emitEvent();
+              };
+              checkboxElement.addEventListener('change', callback);
+              this.listeners.push({element: checkboxElement, callback});  
             });
-        });
-    }
-    emitEvent() {
-        const checkedRarities = this.data
-            .filter((item, index) => this.shadowRoot.querySelector(`#item-${index}`).checked)
-            .map(item => item[0]);
-
-        const event = new CustomEvent('raritiesUpdate', {
-            detail: {
-                checkedRarities
+        }
+        disconnectedCallback() {
+            this.listeners.forEach(({element, callback}) => {
+              element.removeEventListener('change', callback);  
+            });
+            this.listeners = [];  
+        }
+        emitEvent() {
+            const checkedRarities = this.data
+                .filter((item, index) => this.shadowRoot.querySelector(`#item-${index}`).checked)
+                .map(item => item[0]);
+            const event = new CustomEvent('raritiesUpdate', {
+                detail: {
+                    checkedRarities
+                }
             }
-        });
-
+        );
         this.dispatchEvent(event);
         console.log(event.detail);
+    }
+    getStyles() {
+        return `
+          <style>
+            li {
+              color: gray;
+              display: flex;
+              align-items: center;
+            }
+            .checkbox {
+              appearance: none;
+              width: 24px;
+              height: 24px;
+              margin: 12px;
+              position: relative;
+              cursor: pointer;
+            }
+            .checkbox::before {
+              content: "";
+              width: 100%;
+              height: 100%;
+              border: 0.5px solid gray;
+              border-radius: 4px; 
+              position: absolute;
+            }
+            .checkbox:checked::before {
+              background: linear-gradient(rgb(62, 174, 255) -4.13%, rgb(60, 244, 200) 97.72%);
+            }
+            .checkbox::after {
+              content: "";
+              display: none;
+              width: 100%;
+              height: 100%;
+              background: url(${RaritiesComponent.checkIcon}) no-repeat;
+              background-size: 80%;
+              background-position: 70% 55%;
+              position: absolute;
+            }
+            .checkbox:checked::after {
+              display: block;
+            }
+          </style>`
+        ;
     }
 }
